dc2d1088f000437a1fc1ab39195d753bde359116
Duncan Horne <duncan.edwardhorne@gmail.com>
2023-05-25 14:00:22 +0100

    deleted the old JS files

--- pages/_app.js
+++
@@ -1,7 +0,0 @@
-// `pages/_app.js`
-import '../styles/globals.sass';
-import '../styles/themes.sass';
-
-export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
-}
