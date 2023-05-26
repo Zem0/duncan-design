783e87ded358b9073431d1966455340998e39e54
Duncan Horne <duncan.edwardhorne@gmail.com>
2023-05-26 12:26:48 +0100

    Added some useEffect code to retrieve the set themename from loaclStorage

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,16 @@
 // `pages/_app.js`
 import '../styles/globals.sass';
 import '../styles/themes.sass';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
+
+    useEffect(() => {
+        const storedTheme = localStorage.getItem('theme') ?? null
+        if (storedTheme) {
+            document.documentElement.setAttribute('data-theme', storedTheme)
+        }
+    }, [])
+
     return <Component {...pageProps} />;
 }
