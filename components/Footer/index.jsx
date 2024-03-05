9a1e96fe841f67d25dee8bc005194d00a5cd823a
Duncan Horne <duncan.edwardhorne@gmail.com>
2024-03-05 09:27:44 +0000

    Added some styling for active links and added some icons to links in the footer.

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -2,6 +2,7 @@
 import footerStyles from './index.module.sass';
 import utilStyles from '../../styles/utils.module.sass';
 import Music from '../Music';
+import { DribbbleLogo, TwitterLogo, Link, EnvelopeOpen } from '@phosphor-icons/react';
 // import ThemeControl from '../ThemeControl';
 import React from 'react';
 
@@ -18,10 +19,10 @@ export default function Footer() {
                 <div className={`${footerStyles['footer__links']} ${utilStyles.flex} ${utilStyles['flex-row']} ${utilStyles['gap-15']}`}>
                     <ul className={`${footerStyles['footer__links__list']}`}>
                         <li><h6>Elsewhere</h6></li>
-                        <li><a href='https://mastodon.design/@duncandesi9n' target='_blank'>Mastodon</a></li>
-                        <li><a href='https://dribbble.com/DuncanDesign' target='_blank'>Dribbble</a></li>
-                        <li><a href='https://instagram.com/duncandesi9n' target='_blank'>Twitter</a></li>
-                        <li><a href='mailto:duncan@duncandesign.online'>duncan@duncandesign.online</a></li>
+                        <li><a href='https://mastodon.design/@duncandesi9n' target='_blank'><Link weight='fill'/> Mastodon</a></li>
+                        <li><a href='https://dribbble.com/DuncanDesign' target='_blank'><DribbbleLogo weight='fill'/> Dribbble</a></li>
+                        <li><a href='https://instagram.com/duncandesi9n' target='_blank'><TwitterLogo weight='fill'/> Twitter</a></li>
+                        <li><a href='mailto:duncan@duncandesign.online'><EnvelopeOpen weight='fill'/> duncan@duncandesign.online</a></li>
                     </ul>
                 </div>
             </div>
